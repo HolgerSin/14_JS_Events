591accbaaff61823d2487ae7e5c9d9e69a38977d
Peter <schmidt.multimedia@gmail.com>
2021-08-06 12:02:15 +0200

    01 / switchClassName() |switchBtnTxt()

--- events.js
+++ events.js
@@ -1,6 +1,27 @@
 
 /********  Variablen **********/
 
+const btn = document.getElementById("trigBtn");
+
+
+/********  Änderung in View-Schicht **********/
+
+
+// Modul: Umschaltung Klassenamen | Test:
+// switchClassName("night");
+// switchClassName("day");
+function switchClassName(modeStr) {
+    document.body.className = modeStr;
+    document.body.children[0].className = modeStr;
+    document.body.children[1].className = modeStr;
+}
+
+// Modul: Umschaltung BtnTxt | Test:
+// switchBtnTxt("night");
+// switchBtnTxt("day");
+function switchBtnTxt(modeStr) {
+   btn.innerHTML = modeStr;
+}
 
 /* Tools */
 function ausgabe(outputStr) {
